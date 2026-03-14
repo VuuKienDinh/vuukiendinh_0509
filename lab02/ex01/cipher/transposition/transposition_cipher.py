080dfedc1d89e04f1737ee6c8013ac1ed0113eb8
unknown <dinhvuu05@gmail.com>
2026-03-14 22:33:59 +0700

    [add] lab02 web caesar,vigenere,playfair,railfence,transposition

--- lab02/ex01/cipher/transposition/transposition_cipher.py
+++ lab02/ex01/cipher/transposition/transposition_cipher.py
@@ -1,32 +1,35 @@
 class TranspositionCipher:
-
-    def __init__(self):
-        pass
-
-
     def encrypt(self, text, key):
-        encrypted_text = ''
+        # Create a list of empty strings for each column
+        ciphertext = [''] * key
 
+        # Loop through each column in ciphertext
         for col in range(key):
             pointer = col
-
             while pointer < len(text):
-                encrypted_text += text[pointer]
+                ciphertext[col] += text[pointer]
                 pointer += key
 
-        return encrypted_text
-
+        # Convert list into a single string
+        return ''.join(ciphertext)
 
     def decrypt(self, text, key):
-        decrypted_text = [''] * key
-        row, col = 0, 0
+        # Calculate number of columns and rows
+        num_cols = int(len(text) / key)
+        num_rows = key
+        num_shaded_boxes = (num_cols * num_rows) - len(text)
 
+        # Each string in plaintext represents a column
+        plaintext = [''] * num_cols
+
+        col = 0
+        row = 0
         for symbol in text:
-            decrypted_text[col] += symbol
+            plaintext[col] += symbol
             col += 1
 
-            if col == key or (col == key - 1 and row >= len(text) % key):
+            if (col == num_cols) or (col == num_cols - 1 and row >= num_rows - num_shaded_boxes):
                 col = 0
                 row += 1
 
-        return ''.join(decrypted_text)
+        return ''.join(plaintext)
